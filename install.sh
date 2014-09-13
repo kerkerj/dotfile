bfdc9bde1847c91ea048b0e2a4d77d0d8f7e232b
Jerry Huang <jerry.huang@livebricks.com>
2014-09-13 10:41:18 +0800

    fix script

--- install.sh
+++ install.sh
@@ -4,13 +4,11 @@ echo "Copying .tmux file..."
 cd ~
 ln -s ~/dotfile/.tmux.conf ~
 
-echo "Copying .vim folder..."
+echo "Linking .vim folder add .vimrc..."
 cd ~/dotfile
-mv ~/dotfile/.vim/ ~/.vim
-cd ~
-ln -s ~/.vim/.vimrc .
+ln -s ~/dotfile/.vim ~/.vim
+ln -s ~/dotfile/.vim/.vimrc ~/.vimrc
 
-echo "Installing Vundle"
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "Install Vundle by: 'git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim'"
 
 echo "Remember to install Zsh!"
